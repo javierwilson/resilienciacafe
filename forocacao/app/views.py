91390e938c001aeb150137626d569f580547c09e
Javier Wilson <javier.wilson@gmail.com>
2016-06-16 21:44:16 +0000

    is it home?

--- forocacao/app/views.py
+++ forocacao/app/views.py
@@ -29,6 +29,7 @@ class HomeView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
+        context['home'] = True
         context['logos'] = Logo.objects.filter(event=self.object)
         return context
 
